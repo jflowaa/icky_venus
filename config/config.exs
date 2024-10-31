93f77506623693ce483b1b1a90cc09efd7720b13
Jack Flowers <jakaf1@gmail.com>
2024-10-30 21:22:51 -0700

    Add persistance to post created gen server

--- config/config.exs
+++ config/config.exs
@@ -10,3 +10,13 @@ config :number,
     delimiter: ",",
     separator: "."
   ]
+
+config :logger, :default_handler,
+  config: [
+    file: ~c"/root/logs/icky_venus.log",
+    filesync_repeat_interval: 5000,
+    file_check: 5000,
+    max_no_bytes: 10_000_000,
+    max_no_files: 5,
+    compress_on_rotate: true
+  ]
--- lib/blue_sky/post_created_server.ex
+++ lib/blue_sky/post_created_server.ex
@@ -1,13 +1,18 @@
 defmodule BlueSky.PostCreatedServer do
   use GenServer
+  require Logger
+
+  @state_file "post_created_state.json"
 
   def start_link(_) do
     GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
 
   def init(_) do
+    Process.flag(:trap_exit, true)
+    state = read_state_from_file()
     Process.send_after(self(), :register_event, 1000)
-    {:ok, %{}}
+    {:ok, state}
   end
 
   def handle_call(:get_totals, _from, state) do
@@ -28,7 +33,53 @@ defmodule BlueSky.PostCreatedServer do
     {:stop, :normal, state}
   end
 
-  def terminate(_reason, state) do
+  def terminate(reason, state) do
+    write_state_to_file(state)
+    Logger.info("PostCreatedServer is terminating", reason: reason)
     {:ok, state}
+  end
+
+  defp read_state_from_file do
+    state_file_path = get_state_file_path()
+
+    if File.exists?(state_file_path) do
+      case File.read(state_file_path) do
+        {:ok, content} ->
+          case Jason.decode(content) do
+            {:ok, state} ->
+              state
+
+            {:error, reason} ->
+              Logger.error("Failed to decode JSON from state file", reason: reason)
+              %{}
+          end
+
+        {:error, reason} ->
+          Logger.error("Failed to read state file", reason: reason)
+          %{}
+      end
+    else
+      %{}
+    end
+  end
+
+  defp write_state_to_file(state) do
+    state
+    |> Jason.encode!()
+    |> (&File.write!(get_state_file_path(), &1)).()
+  rescue
+    e in File.Error ->
+      Logger.error("Failed to write state file", exception: e)
+      File.mkdir_p!(Path.dirname(get_state_file_path()))
+      Logger.info("Attempting to write state file again after creating directory")
+
+      state
+      |> Jason.encode!()
+      |> (&File.write!(get_state_file_path(), &1)).()
+  end
+
+  defp get_state_file_path do
+    config_dir = :filename.basedir(:user_config, to_string(:icky_venus))
+    Path.join([config_dir, @state_file])
   end
 end
